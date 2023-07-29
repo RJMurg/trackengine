a5a0b4c92d1bd0db58e9196009e0400dc936fced
Ruán <ruan@murgatroyd.me>
2023-07-29 12:02:22 +0100

    Finished network endpoint

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 PORT = process.env.PORT || 3000;
 
-app.get('/status', (req, res) => {
+app.post('/status', (req, res) => {
     var start = Date.now();
     api.getStations();
     var end = Date.now();
@@ -23,26 +23,37 @@ app.get('/status', (req, res) => {
     res.status(200).json({ message: 'ok', status: 200, timestamp: currentTime, delay: externalDelay + 'ms' });
 });
 
-app.post('/network', (req, res) => {
+app.post('/network', async (req, res) => {
     // Return the following:
     // 1. Amount of stations
     // 2. Amount of trains
+    // 3. Amount of running trains
     // 3. Average delay of all trains
 
-    var stations = api.getStations();
-    var trains = api.getTrains();
+    var stations = await api.getStations();
+    var trains = await api.getTrains();
 
     var stationCount = stations.length;
-    var trainCount = trains.length;
+    var allTrainCount = trains.length;
+
+    var runningTrainCount = 0;
+
+    for(var i = 0; i < trains.length; i++){
+        if(trains[i].currentStatus == 'Running'){
+            runningTrainCount++;
+        }
+    }
 
     var totalDelay = 0;
     for(var i = 0; i < trains.length; i++){
-        totalDelay += trains[i].delay;
+        if(trains[i].delay != null){
+            totalDelay += parseInt(trains[i].delay), 0;
+        }
     }
 
-    var averageDelay = totalDelay / trains.length;
+    var averageDelay = Math.round(totalDelay / trains.length);
 
-    res.status(200).json({ stations: stationCount, trains: trainCount, averageDelay: averageDelay, status: 200 });
+    res.status(200).json({ stations: stationCount, trains: allTrainCount, running: runningTrainCount, averageDelay: averageDelay, status: 200 });
 
 });
 
